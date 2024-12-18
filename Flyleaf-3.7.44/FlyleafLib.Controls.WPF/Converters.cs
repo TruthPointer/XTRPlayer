dc88416c05ddaa78600a3437a23883ad8870bf4f
TruthPointer <OneTruthPointer@outlook.com>
2024-12-18 19:56:23 +0800

    Add files via upload

---
+++ Flyleaf-3.7.44/FlyleafLib.Controls.WPF/Converters.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FlyleafLib.Controls.WPF
+{
+    public class QualityToLevelsConverter : IValueConverter//20240216
+    {
+        public enum Qualities
+        {
+            None,
+            Low,
+            Med,
+            High
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string name = (string)value;
+            if (name == "低频宽")
+                return Qualities.Low;
+            else if (name == "中频宽")
+                return Qualities.Med;
+            else if (name == "高频宽")
+                return Qualities.High;
+            else
+                return Qualities.None;//暂时定为Low，实为 未知
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+	}
+
+    public class QualityToIsCheckedConverter : IMultiValueConverter//20240216
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2)
+            {
+                Utils.Log("----> " + values[1] as string);
+            }
+
+            if (values.Length != 2 || !(values[0] is string) || !(values[1] is string))
+                return false;
+            string newName = (string)values[0];
+            string menuItemName = (string)values[1];
+            return newName == menuItemName;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+
+    public class VolumeToLevelsConverter : IValueConverter
+    {
+        public enum Volumes
+        {
+            Mute,
+            Low,
+            Med,
+            High
+        }
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)     
+        {
+            int volume = (int) value;
+
+            if (volume == 0)
+                return Volumes.Mute;
+            else if (volume > 99)
+                return Volumes.High;
+            else if (volume > 49)
+                return Volumes.Med;
+            else
+                return Volumes.Low;
+        }
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+	}
+
+    public class ErrorMsgWithChineseTipConverter : IValueConverter//20240216
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string newMsg = (string) value;
+            return string.IsNullOrEmpty(newMsg) ? "" : "错误：" + newMsg;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+    }
+
+    public class CheckNullConverter : IMultiValueConverter
+    {
+		public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)     
+        {
+            if (value == null) return true;
+            if (value[0] == null || value[0] == System.Windows.DependencyProperty.UnsetValue) return true;
+            if (value[1] == null || value[1] == System.Windows.DependencyProperty.UnsetValue) return true;
+
+            return !((IDictionary)value[0]).Contains(value[1]);
+        }
+		public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+	}
+
+    public class BooleanAllConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            => values.OfType<bool>().All(b => b);
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class BooleanConverter<T> : IValueConverter
+    {
+        public BooleanConverter(T trueValue, T falseValue)
+        {
+            TrueValue = trueValue;
+            FalseValue = falseValue;
+        }
+
+        public T TrueValue { get; set; }
+        public T FalseValue { get; set; }
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool boolValue && boolValue ? TrueValue : FalseValue;
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is T tValue && System.Collections.Generic.EqualityComparer<T>.Default.Equals(tValue, TrueValue);
+    }
+    public class InvertBooleanConverter : BooleanConverter<bool>
+    {
+        public InvertBooleanConverter()
+            : base(false, true)
+        {
+        }
+    }
+
+    [ValueConversion(typeof(double), typeof(double), ParameterType = typeof(Orientation))]
+    public class SliderValueLabelPositionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is Orientation orientation && value is double width)
+            {
+                const double halfGripWidth = 9.0;
+                const double margin = 4.0;
+
+                switch (orientation)
+                {
+                    case Orientation.Horizontal:
+                        return (-width * 0.5) + halfGripWidth;
+
+                    case Orientation.Vertical:
+                        return -width - margin;
+
+                   default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            return 0.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class SumConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double sum = 0;
+
+            if (values == null) return sum;
+
+            foreach (object value in values)
+            {
+                if (value == System.Windows.DependencyProperty.UnsetValue) continue;
+                sum += (double)value;
+            }
+
+            return sum;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class PlaylistItemsConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"播放列表 ({values[0]}/{values[1]})";//20240216 Playlist
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class GetDictionaryItemConverter : IMultiValueConverter
+    {
+		public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)     
+        {
+            if (value == null) return null;
+            if (value[0] == null || value[0] == System.Windows.DependencyProperty.UnsetValue) return null;
+            if (value[1] == null || value[1] == System.Windows.DependencyProperty.UnsetValue) return null;
+
+            return ((IDictionary)value[0])[value[1]];
+        }
+		public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture) { throw new NotImplementedException(); }
+	}
+
+    public class MarginConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new System.Windows.Thickness(0, System.Convert.ToDouble(value), 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    [ValueConversion(typeof(Color), typeof(Brush))]
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color)
+            {
+                return new SolidColorBrush(color);
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SolidColorBrush brush)
+            {
+                return brush.Color;
+            }
+            return default(Color);
+        }
+    }
+
+    public class BrushToHexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+            string lowerHexString(int i) => i.ToString("X2").ToLower();
+            var brush = (SolidColorBrush)value;
+            var hex = lowerHexString(brush.Color.R) +
+                      lowerHexString(brush.Color.G) +
+                      lowerHexString(brush.Color.B);
+            return "#" + hex;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class ColorToHexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+
+            string lowerHexString(int i) => i.ToString("X2").ToLower();
+            Color color = (Color)value;
+            var hex = lowerHexString(color.R) +
+                      lowerHexString(color.G) +
+                      lowerHexString(color.B);
+            return hex;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return ColorConverter.ConvertFromString("#" + value.ToString());
+            } catch(Exception) { }
+
+            return Binding.DoNothing;
+        }
+            
+    }
+}
---
+++ Flyleaf-3.7.44/README.md
@@ -0,0 +1,116 @@
+﻿# *Flyleaf v3.7*: Media Player .NET Library for WinUI 3/WPF/WinForms (based on FFmpeg/DirectX)
+
+![alt text](Images/Flyleafv3.6.png)
+
+---
+
+>Notes<br/>
+>1. FlyleafLib's releases will be on [NuGet](https://www.nuget.org/packages?q=flyleaf)
+>2. Compiled samples will be on [GitHub releases](https://github.com/SuRGeoNix/Flyleaf/releases)
+>3. Documentation will be on [Wiki](https://github.com/SuRGeoNix/Flyleaf/wiki) and [Samples](https://github.com/SuRGeoNix/Flyleaf/tree/master/Samples) within the solution
+
+# [Overview]
+
+✅ **Play Everything** <sub>(Audio, Videos, Images, Playlists over any Protocol)</sub>
+
+- *Extends FFmpeg's supported protocols and formats with additional plugins <sub>(YoutubeDL, TorrentBitSwarm)</sub>*
+- *Accepts Custom I/O Streams and Plugins to handle non-standard protocols / formats*
+	
+✅ **Play it Smoothly** <sub>(Even with high resolutions 4K / HDR)</sub>
+
+- *Coded from scratch to gain the best possible performance with FFmpeg & DirectX using video acceleration and custom pixel shaders*
+- *Threading implementation with efficient cancellation which allows fast open, play, pause, stop, seek and stream switching*
+	
+✅ **Develop it Easy**
+
+- *Provides a DPI aware, hardware accelerated Direct3D Surface (FlyleafHost) which can be hosted as normal control to your application and easily develop above it your own transparent overlay content*
+- *All the implementation uses UI notifications (PropertyChanged / ObservableCollection etc.) so you can use it as a ViewModel directly*    
+- *For WPF provides a Control (FlyleafME) with all the basic UI sub-controls (Bar, Settings, Popup menu) and can be customized with style / control template overrides*
+
+# [Features]
+
+### **FFmpeg**
+- *HLS Live Seeking* <sub>Might the 1st FFmpeg player which does that</sub>
+- *Pached for [HLS issue](https://patchwork.ffmpeg.org/project/ffmpeg/list/?series=1018)* <sub>Use recommended FFmpeg libraries which can be found on GitHub releases</sub>
+- *Capture Devices* <sub>Pass the format, input and options with a single Url eg. fmt://gdigrab?desktop&framerate=30</sub>
+- *Supports FFmpeg v5+*
+
+### **Playback**
+- *Open / Play / Pause / Stop*
+- *Speed / Reverse / Zero-Low Latency*
+- *Seek Backward / Forward (Short / Large Step)*
+- *Seek to Time / Seek to Frame / Seek to Chapter / Frame Stepping*
+
+### **Video**
+- *Enable / Disable*
+- *Device Preference*
+- *Aspect Ratio (Keep / Fill / Custom)*
+- *Deinterlace (Bob)*
+- *HDR to SDR (Aces / Hable / Reinhard)* <sub>[BT2020 / SMPTE 2084 to BT709]</sub>
+- *Pan Move / Zoom / Rotate / HFlip-VFlip (Replica Renderer/Interactive Zoom)*
+- *Record / Snapshot*
+- *Video Acceleration*
+- *Video Filters (Brightness / Contrast / Hue / Saturation)*
+- *Video Processors (Flyleaf / D3D11)*
+- *VSync*
+- *Zero-Copy*
+
+### **Audio**
+- *Enable / Disable*
+- *Device Preference*
+- *Add / Remove Delay (Short / Large Step)*
+- *Volume (Up / Down / Mute)*
+- *Languages support* <sub>System's default languages as priorities for audio streams</sub>
+
+### **Subtitles**
+- *Enable / Disable*
+- *Add / Remove Delay (Short / Large Step)*
+- *Advanced Character Detection and Convert to UTF-8* <sub>SubtitlesConverter plugin</sub>
+- *Languages support* <sub>System's default languages as priorities for subtitles streams</sub>
+
+### **UI Control (FlyleafHost)**<sub>WPF / WinUI &amp; WinForms (Partially)</sub>
+- *Attach / Detach*
+- *Activity / Idle Mode*
+- *Drag Move (Self / Owner)*
+- *Drag & Drop Swap*
+- *Drag & Drop Open*
+- *Full / Normal Screen*
+- *Resize / Resize & Keep Ratio*
+- *Z-Order*
+
+### **UI Control (FlyleafME)** <sub>WPF Only</sub>
+- *Flyleaf Bar Control / Slider*
+- *Flyleaf Popup Menu*
+- *Flyleaf Settings Dialog*
+- *Color Themes* <sub>Based on Material Design in XAML</sub>
+- *Style / Control Template Customization*
+
+### **Plugins**
+- *OpenSubtitlesOrg* <sub>Search & Download for online Subtitles</sub>
+- *SubtitlesConverter* <sub>Detect & Convert the input's charset to UTF-8</sub>
+- *TorrentBitSwarm* <sub>Play a media from torrent without the need to download it completely</sub>
+- *YoutubeDL* <sub>Play web media that are not accessible directly with HTTP(s)</sub>
+
+### Misc.
+- *Mouse & Key Bindings* <sub>All the implementation supports customizable mouse & key bindings which can be assigned to an embedded or a custom actions (find defaults [here](https://github.com/SuRGeoNix/Flyleaf/wiki/Player-(Key-&-Mouse-Bindings)))</sub>
+- *Audio Player* <sub>Can be used as an audio player only without the need of UI Control</sub>
+- *Downloader / Remuxer* <sub>The library can be used also for downloading & remuxing</sub>
+- *Extractor* <sub>The library can be used also for extracting video frames (supports also by X frames Step)</sub>
+
+# [Thanks to]
+
+*Flyleaf wouldn't exist without them!*
+
+* *For the Core*
+  * ***[FFmpeg](http://ffmpeg.org/)*** / ***[FFmpeg.AutoGen](https://github.com/Ruslan-B/FFmpeg.AutoGen/)***
+  * ***[Vortice](https://github.com/amerkoleci/Vortice.Windows)***
+  * *Major open source media players* ***[VLC](https://github.com/videolan/vlc)***, ***[Kodi](https://github.com/xbmc/xbmc)***, ***[MPV](https://github.com/mpv-player/mpv)***, ***[FFplay](https://github.com/FFmpeg/FFmpeg/blob/master/fftools/ffplay.c)***
+
+* *For the UI*
+  * ***[Dragablz](https://github.com/ButchersBoy/Dragablz)***
+  * ***[MaterialDesign Colors & Themes](https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit/)***
+
+* *For the Plugins*
+  * ***[BitSwarm](https://github.com/SuRGeoNix/BitSwarm)***
+  * ***[OpenSubtitles.org](https://www.opensubtitles.org/)***
+  * ***[YT-DLP](https://github.com/yt-dlp/yt-dlp)***
